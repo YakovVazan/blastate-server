d76cc63263bc51e29693aa343707e3cac54f4873
YakovVazan <yakovvazan@gmail.com>
2024-03-28 13:09:40 +0200

    fix: moved secret to a .env file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,3 +1,4 @@
+// app.module.ts
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -6,16 +7,22 @@ import { UsersModule } from './users/users.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { CitiesModule } from './cities/cities.module';
 import { AlertsModule } from './alerts/alerts.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     AuthModule,
     UsersModule,
     CitiesModule,
     AlertsModule,
-    MongooseModule.forRoot(
-      'mongodb+srv://yakovvazan:Ku2orSNr9ygUq0qn@realblastate.nibi1ky.mongodb.net/',
-    ),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: `mongodb+srv://${configService.get('MONGODB_USER')}:${configService.get('MONGODB_PASSWORD')}@realblastate.nibi1ky.mongodb.net/`,
+      }),
+      inject: [ConfigService],
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
